6748c8a863a16db20d39053e816fc7685994e01e
comunidadudp <comunidad.udp.shared@gmail.com>
2025-09-19 00:21:42 -0500

    auth controller completed

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using RbacApi.DTOs;
+using RbacApi.Responses;
 using RbacApi.Services.Interfaces;
 
 namespace RbacApi.Controllers;
@@ -12,8 +13,34 @@ public class AuthController(IAuthService authService) : ControllerBase
 
 
     [HttpPost("register")]
-    public async Task<IActionResult> Register([FromBody] RegisterRequest request)
+    public async Task<ApiResponseBase> Register([FromBody] RegisterRequest request)
     {
-        return Ok(await _authService.RegisterAsync(request));
+        var response = await _authService.RegisterAsync(request);
+        HttpContext.Response.StatusCode = response.Status;
+        return response;
+    }
+
+    [HttpPost("login")]
+    public async Task<ApiResponseBase> Login([FromBody] LoginRequest request)
+    {
+        var response = await _authService.LoginAsync(request);
+        HttpContext.Response.StatusCode = response.Status;
+        return response;
+    }
+
+    [HttpPost("refresh")]
+    public async Task<ApiResponseBase> Refresh([FromBody] RefreshRequest request)
+    {
+        var response = await _authService.RefreshAsync(request);
+        HttpContext.Response.StatusCode = response.Status;
+        return response;
+    }
+
+    [HttpPost("revoke")]
+    public async Task<ApiResponseBase> Revoke([FromBody] RefreshRequest request)
+    {
+        var response = await _authService.RevokeRefreshTokenAsync(request);
+        HttpContext.Response.StatusCode = response.Status;
+        return response;
     }
 }
